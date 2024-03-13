ff98a81e7ae5b5d6254391e6ec424ba16dc60897
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-03-13 21:03:37 +0530

    Update class.py

--- plugins/class.py
+++ plugins/class.py
@@ -3,47 +3,44 @@ from bot import Bot
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
-async def aditya_handler(client: Bot, query: CallbackQuery):
+async def callback_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "aditya":
-        await query.message.edit_text(
-            text = f"gendu",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+        await aditya_handler(client, query)
+    elif data == "radiux":
+        await radiux_handler(client, query)
+
+async def aditya_handler(client: Bot, query: CallbackQuery):
+    await query.message.edit_text(
+        text = f"gendu",
+        disable_web_page_preview = True,
+        reply_markup = InlineKeyboardMarkup(
+            [
                 [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
-            )
+            ]
         )
-    elif data == "close":
-        await query.message.delete()
-        try:
-            await query.message.reply_to_message.delete()
-        except:
-            pass
-
-
+    )
 
 async def radiux_handler(client: Bot, query: CallbackQuery):
-    data = query.data
-    if data == "radiux":
-        await query.message.edit_text(
-            text = f" experiment",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+    await query.message.edit_text(
+        text = f" experiment",
+        disable_web_page_preview = True,
+        reply_markup = InlineKeyboardMarkup(
+            [
                 [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
-            )
+            ]
         )
-    elif data == "close":
+    )
+
+@Bot.on_callback_query()
+async def close_handler(client: Bot, query: CallbackQuery):
+    if query.data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
         except:
             pass
-
