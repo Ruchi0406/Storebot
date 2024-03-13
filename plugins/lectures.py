275680d132c9495a43ea544f9fd5b2a4d4650e2d
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-03-13 19:50:20 +0530

    Update lectures.py

--- plugins/lectures.py
+++ plugins/lectures.py
@@ -11,14 +11,15 @@ from database.database import add_user
 async def help_command(client: Client, message):
     # Customizable help message
     help_text = """
-   Serious Question :
-   Aman Aditya ka beta hokar bhi gendu kyu hai ??
+  Select your class --
     """
 
     # Inline button that links to your channel
     reply_markup = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("Visit Channel 📢", url="https://t.me/jeestudyroom")],
+            [InlineKeyboardButton("class 11", callback_data= "aditya")
+            ]
         ]
     )
 
