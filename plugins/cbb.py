27db0be849ea0d9da008c70f8768cb336371b435
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2024-03-20 21:26:53 +0530

    add parsemode

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -3,6 +3,7 @@
 from pyrogram import __version__
 from bot import Bot
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram import enums
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
@@ -10,6 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     if data == "about":
         await query.message.edit_text(
             text = f" Developed By - Ordinators of [JEE STUDY ROOM Org.](https://t.me/Jeestudyroom)",
+            parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
